46b2198a965fc3b6a285c51c8fe79da69a73cd78
yvonnesjy <stonfish@hotmail.com>
2017-02-24 18:13:00 +0800

    Restructured index

--- web/index.php
+++ web/index.php
@@ -2,44 +2,55 @@
 include 'utils.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require('../vendor/autoload.php');
 
+require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
-
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Herrera\Pdo\PdoServiceProvider(),
-   array(
-       'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/'),
-       'pdo.host' => $dbopts["host"],
-       'pdo.port' => $dbopts["port"],
-       'pdo.username' => $dbopts["user"],
-       'pdo.password' => $dbopts["pass"]
-   )
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
 );
 
+// Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => 'php://stderr',
+  'monolog.logfile' => 'php://stderr',
 ));
 
+// Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
+// Our web handlers
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
 
-$query = "select * from sisters";
 $app->get('/db/', function() use($app) {
-    $st = $app['pdo']->prepare($query);
-    $st->execute();
-    
-    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-        $app['monolog']->addDebug('Row ' . $row['first'].$row['sister'].$row['number']);
-        echo $row['first'].$row['sister'].$row['number'];
-    }
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  $query = "select * from sisters";
+  $app->get('/db/', function() use($app) {
+      $st = $app['pdo']->prepare($query);
+      $st->execute();
+      
+      while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+          $app['monolog']->addDebug('Row ' . $row['first'].$row['sister'].$row['number']);
+          $names[] = $row;
+      }
+  });
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
 });
 
 $app->run();
