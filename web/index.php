1c991caa18ee4fadd2625bf261d4289fd6c7f13f
yvonnesjy <stonfish@hotmail.com>
2017-02-28 03:10:05 +0800

    Fixed index

--- web/index.php
+++ web/index.php
@@ -9,7 +9,8 @@ $app['debug'] = true;
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Herrera\Pdo\PdoServiceProvider(),
   array(
-    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/'),
+    'pdo.host' => $dbopts["host"],
     'pdo.port' => $dbopts["port"],
     'pdo.username' => $dbopts["user"],
     'pdo.password' => $dbopts["pass"]
@@ -22,35 +23,30 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 ));
 
 // // Register view rendering
-// $app->register(new Silex\Provider\TwigServiceProvider(), array(
-//     'twig.path' => __DIR__.'/views',
-// ));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
 
 // // Our web handlers
-// $app->get('/', function() use($app) {
-//   $app['monolog']->addDebug('logging output.');
-//   return $app['twig']->render('index.twig');
-// });
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
 
+$query = "select * from sisters";
 $app->get('/db/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT name FROM test_table');
-  $st->execute();
+    $st = $app['pdo']->prepare($query);
+    $st->execute();
+    
+    $names = array();
+    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+        $app['monolog']->addDebug('Row ' . $row['first'].$row['sister'].$row['number']);
+        $names[] = $row;
+    }
 
-  $names = array();
-  $query = "select * from sisters";
-  $app->get('/db/', function() use($app) {
-      $st = $app['pdo']->prepare($query);
-      $st->execute();
-      
-      while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-          $app['monolog']->addDebug('Row ' . $row['first'].$row['sister'].$row['number']);
-          $names[] = $row;
-      }
-  });
-
-  // return $app['twig']->render('database.twig', array(
-  //   'names' => $names
-  // ));
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
 });
 
 $app->run();
