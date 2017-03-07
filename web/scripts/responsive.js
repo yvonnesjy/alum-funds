db336964317e74d9839deda77175b8a564afcb24
yvonnesjy <stonfish@hotmail.com>
2017-03-07 16:48:26 +0800

    Added responsive design. Added donate and close buttons.

---
+++ web/scripts/responsive.js
@@ -0,0 +1,40 @@
+if (window.innerHeight / window.innerWidth < 1) {
+    leftPanel = document.getElementById('left-panel');
+    rightPanel = document.getElementById('right-panel');
+    rowPanel = document.getElementsByClassName('row')[0];
+    donateBtn = document.getElementById('donate-button');
+    closeBtn = document.getElementById('close-button');
+
+    leftPanel.style.height = "100%";
+    leftPanel.style.width = "66.66%";
+
+    rightPanel.style.height = "100%";
+    rightPanel.style.width = "33.33%";
+    rightPanel.style.boxShadow = "-3px 4px 14px 0px #888888";
+
+    rowPanel.style.display = "-webkit-flex";
+
+    donateBtn.setAttribute('class', 'hidden');
+    closeBtn.setAttribute('class', 'hidden');
+} else {
+    leftPanel = document.getElementById('left-panel');
+    rightPanel = document.getElementById('right-panel');
+    donateBtn = document.getElementById('donate-button');
+    closeBtn = document.getElementById('close-button');
+
+    leftPanel.style.height = "100%";
+    leftPanel.style.width = "100%";
+
+    rightPanel.setAttribute('class', 'hidden');
+    closeBtn.setAttribute('class', 'visible');
+
+    donateBtn.onclick = function() {
+        leftPanel.setAttribute('class', 'hidden');
+        rightPanel.setAttribute('class', 'visible');
+    };
+
+    closeBtn.onclick = function() {
+        leftPanel.setAttribute('class', 'visible');
+        rightPanel.setAttribute('class', 'hidden');
+    };
+}
